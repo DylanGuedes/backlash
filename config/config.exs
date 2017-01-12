9352711fab855619a8014779c66fcac5d973488b
DylanGuedes <djmgguedes@gmail.com>
2017-01-12 18:16:38 -0200

    Initial commit
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :labyrinth,
+  ecto_repos: [Labyrinth.Repo]
+
+# Configures the endpoint
+config :labyrinth, Labyrinth.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "C+qz8m731aAxRQnE9QGQbzhx8KZ8lCc9POv6Yao2UO5VoOs5WJe5mlTtyuSqTjS8",
+  render_errors: [view: Labyrinth.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Labyrinth.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ test/test_helper.exs
@@ -0,0 +1,4 @@
+ExUnit.start
+
+Ecto.Adapters.SQL.Sandbox.mode(Labyrinth.Repo, :manual)
+
